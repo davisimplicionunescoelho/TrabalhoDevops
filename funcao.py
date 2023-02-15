59d06322e460d1ce86c4a4184bb83adb90002310
davisimplicionunescoelho <davisimplicio104@gmail.com>
2023-02-15 17:00:47 -0300

    subindo arquivo .py com alteração na branch develop

--- funcao.py
+++ funcao.py
@@ -1,6 +1,12 @@
+# Como fazer função no python
+
 def saida_de_dados(text):
+    #multiplicando string
     print("*"*10)
+
     print(text)
-    print("+"*10)
+
+    print("+"*11)
     
+#mostrando a saída de dados
 saida_de_dados("Marcelo Grilo")
